e5bf5818551bc63cf816f30ed7988e78e6669464
Romen, Ter Gazarian <romen.tergazarian@gmail.com>
2022-07-05 17:57:16 -0300

    [1]Deploy

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# js-portfolio
+# Package JS
+
+In this project i used the principals utils of package for have a clean and optimized code.
+
+I played with alias and used loaders for images; 
+
+configurate dev a build mode; 
+
+ad some scripts in package.json (like watch and mode production);
+
+create a .env  file, and for public view, env-example;
+
+and many other functions in package js
--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,7 @@
-import getData from '../utils/getData.js';
+import getData from '@utils/getData.js';
+import github from '@images/github.png'
+import twitter from '@images/twitter.png'
+import instagram from '@images/instagram.png'
 
 const Template = async () => {
   const data = await getData();
@@ -23,14 +26,14 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
-            <img src="../src/assets/images/twitter.png" />
+          <a href="${twitter}">
+            <img src="assets/images/twitter.png" />
           </a>
-          <a href="https://github.com/gndx">
-            <img src="../src/assets/images/github.png" />
+          <a href="${github}">
+            <img src="assets/images/github.png" />
           </a>
-          <a href="https://instagram.com/gndx">
-            <img src="../src/assets/images/instagram.png" />
+          <a href="${instagram}">
+            <img src="assets/images/instagram.png" />
           </a>
         </div>
       </div>
